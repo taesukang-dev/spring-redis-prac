4cdbf64b125df782b9b1c9d0c5157be54d699677
Tae-Su Kang <mtae616@naver.com>
2022-10-15 12:57:08 +0900

    add simple get/set model

--- src/main/java/com/example/redis/RedisApplication.java
+++ src/main/java/com/example/redis/RedisApplication.java
@@ -2,14 +2,8 @@ package com.example.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class,
-		RedisAutoConfiguration.class
-})
-
+@SpringBootApplication
 public class RedisApplication {
 
 	public static void main(String[] args) {
